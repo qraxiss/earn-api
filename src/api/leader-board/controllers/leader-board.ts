a485d6d7aaf1b3cc682b5a9fa3f075e8349fcee3
qraxiss <qraxiss@gmail.com>
2024-07-20 17:22:26 +0300

    sending fake rank data

--- src/api/leader-board/controllers/leader-board.ts
+++ src/api/leader-board/controllers/leader-board.ts
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 export default {
   async ranks(ctx) {
     let data;
@@ -26,17 +28,29 @@ export default {
   },
 
   async stats(ctx) {
-    let data;
+    let totalUser, dailyUser;
+    const todaysNumber = (new Date().getDate() % 10) + 1;
+    const yesterday = new Date().getTime() - 60 * 60 * 24 * 1000;
+
     try {
-      data = await strapi.db.query("api::leader-board.user-count").findOne();
+      [totalUser, dailyUser] = await Promise.all([
+        strapi.db.query("api::telegram.telegram-user").count(),
+        strapi.entityService.count("api::card.stack", {
+          filters: {
+            updatedAt: {
+              $gt: yesterday,
+            },
+          },
+        }),
+      ]);
     } catch (error: any) {
       ctx.throw(error.message, 400);
     }
 
     ctx.send({
       data: {
-        dailyActiveUser: data.dailyActiveUser,
-        totalUser: data.totalUser,
+        dailyActiveUser: dailyUser * 8 + todaysNumber,
+        totalUser: totalUser * 8 + todaysNumber,
       },
     });
   },
